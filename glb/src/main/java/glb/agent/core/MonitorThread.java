f7d1cd0521d7012e929a701dfabd05cb767cbf96
cqu <qchenhao@gmail.com>
2016-10-12 11:36:56 +1100

    thread OK

--- glb/src/main/java/glb/agent/core/MonitorThread.java
+++ glb/src/main/java/glb/agent/core/MonitorThread.java
@@ -1,5 +1,43 @@
 package glb.agent.core;
 
-public class MonitorThread {
+import java.util.Queue;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import glb.agent.core.dc.DCManager;
+import glb.agent.core.dc.LocalDCStatus;
+import glb.agent.monitor.Measure;
+import glb.agent.monitor.Monitor;
+import glib.agent.event.Event;
+import glib.agent.event.LocalDCStatusUpdateEvent;
+
+public class MonitorThread implements Runnable {
+	
+	private Monitor monitor;
+	private Logger log = LogManager.getLogger(MonitorThread.class);
+	public MonitorThread(Monitor monitor) {
+		this.monitor = monitor;
+	}
+	
+	@Override
+	public void run() {
+		
+		while (true) {
+			try {
+				Measure measure = monitor.monitor();
+				DCManager dcManager = DCManager.getDCManager();
+				LocalDCStatus localDCStatus = dcManager.getLocalDCStatus();
+				localDCStatus.update(measure.getCapacity(), measure.getLoad());
+				LocalDCStatusUpdateEvent event = new LocalDCStatusUpdateEvent();
+				Queue<Event> eventQueue = EventQueue.getEventQueue();
+				eventQueue.add(event);
+				eventQueue.notifyAll();
+				Thread.sleep(2000);
+			} catch (Exception e) {
+				log.catching(e);
+			}
+		}
+	}
 
 }
